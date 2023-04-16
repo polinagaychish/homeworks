41adc0891d32f5ff117ba4a943ed72d582994a90
Polina Gaychish <polina.gaychish@gmail.com>
2023-04-17 00:08:24 +0300

    03

---
+++ Задача3/Program.cs
@@ -0,0 +1,12 @@
+﻿/*Console.Write("Введите число: ");
+int x = int.Parse(Console.ReadLine()!);
+if (x % 2 ==0)
+{
+    Console.Write(x);
+    Console.Write(" четное");
+}
+else
+{
+    Console.Write(x);
+    Console.Write(" нечетное");
+}*/

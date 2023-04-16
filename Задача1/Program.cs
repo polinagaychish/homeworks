5b2b9f2dd9bdf192d5977815440f4359574201fa
Polina Gaychish <polina.gaychish@gmail.com>
2023-04-16 23:47:15 +0300

    01

---
+++ Задача1/Program.cs
@@ -0,0 +1,24 @@
+﻿Console.Write("ВВедите число 1: ");
+int x = int.Parse(Console.ReadLine()!);
+Console.Write("ВВедите число 2: ");
+int y = int.Parse(Console.ReadLine()!);
+if (x < y)
+{
+    Console.Write("Наименьшее: ");
+    Console.Write(x);
+}
+else
+{
+    Console.Write(" Наибольшее: ");
+    Console.Write(x);
+}
+if (y < x)
+{
+    Console.Write("Наименьшее: ");
+    Console.Write(y);
+}
+else
+{
+    Console.Write(" Наибольшее: ");
+    Console.Write(y);
+}
